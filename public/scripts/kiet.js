7cfe2991490742ba1064495fabafdec05d0e9802
Kiet Nguyen <kietnvt@zigexn.vn>
2016-01-08 16:31:51 +0700

    Init tutorial

---
+++ public/scripts/kiet.js
@@ -0,0 +1,50 @@
+var Comment = React.createClass({
+  render: function(){
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function(){
+    return (
+      <div className="commentList">
+        <Comment author="Nguyen">This is one comment</Comment>
+        <Comment author="Nguyen">This is another comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function(){
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
