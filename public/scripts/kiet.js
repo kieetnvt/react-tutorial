3b9fa7ae3570dd572327ff903f980ce1ef934524
Kiet Nguyen <kietnvt@zigexn.vn>
2016-01-12 16:59:14 +0700

    complete tutorial

--- public/scripts/kiet.js
+++ public/scripts/kiet.js
@@ -1,9 +1,15 @@
+var static_data_comments = [
+  {id: 1, author: "Nguyen", text: "This is first comment"},
+  {id: 2, author: "Vo", text: "This is second comment"},
+  {id: 3, author: "Tuan", text: "This is third comment"},
+  {id: 4, author: "Kiet", text: "This is fourth comment"},
+];
+
 var Comment = React.createClass({
   rawMarkup: function(){
     var rawMarkup = marked(this.props.children.toString(), {sanitize: true})
     return {__html: rawMarkup}
   },
-
   render: function(){
     return (
       <div className="comment">
@@ -18,38 +24,117 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
   render: function(){
+    var commentNodes = this.props.data.map(function(comment){
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Nguyen">This is one comment</Comment>
-        <Comment author="Nguyen">This is another comment</Comment>
+        {commentNodes}
       </div>
     );
   }
 });
 
 var CommentForm = React.createClass({
+  getInitialState: function(){
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e){
+    this.setState({author: e.target.value})
+  },
+  handleTextChange: function(e){
+    this.setState({text: e.target.value})
+  },
+  handleSubmit: function(e){
+    e.preventDefault();
+    console.log(this.state)
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    };
+    this.props.onCommentSubmit({author: author, text: text});
+    this.setState({author: '', text: ''});
+  },
   render: function(){
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function(){
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleCommentSubmit: function(comment){
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function(){
+    return {data: []};
+  },
+  componentDidMount: function(){
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval)
+  },
   render: function(){
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
-        <CommentForm />
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
       </div>
     );
   }
 });
 
+// load_static_data_comments
+// ReactDOM.render(
+//   <CommentBox data={static_data_comments} />,
+//   document.getElementById('content')
+// );
+
+// dynamic_load_data_from_server
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox url="/api/comments" pollInterval={2000} />,
   document.getElementById('content')
 );
